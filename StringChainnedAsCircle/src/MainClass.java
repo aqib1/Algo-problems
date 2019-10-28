d66fdcc7047e58669e477168749a38a77deadc4e
Aqib Javed <aqibbutt3078@gmail.com>
2019-10-28 11:18:22 +0500

    ChainnedArray of string

---
+++ StringChainnedAsCircle/src/MainClass.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class MainClass {
+
+	public static boolean chainedArray(List<String> data) {
+		int size = data.size();
+		if (size == 1)
+			return true;
+		Collections.sort(data);
+		int first = 0;
+		int last = 1;
+		while (last < data.size()) {
+			String firstWord = data.get(first);
+			String lastWord = data.get(last);
+			if (firstWord.charAt(firstWord.length() - 1) != lastWord.charAt(0)) {
+				return false;
+			}
+			first++;
+			last++;
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(chainedArray(new ArrayList<>(Arrays.asList("ijk"))));
+	}
+
+}
